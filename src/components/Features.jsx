5af7101ac78149b5355a3daf7d64d713164960eb
samiarpita <arpitarahmansamia@gmail.com>
2026-06-08 22:13:50 +0600

    update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,19 +5,19 @@ function Features() {
       <div className="row text-center">
 
         <div className="col-md-4">
-          <ShieldCheck size={50} className="mb-3 text-primary" />
+          <ShieldCheck size={25} className="mb-3 text-primary" />
           <h3>Secure</h3>
           <p>Strong encryption and random generation.</p>
         </div>
 
         <div className="col-md-4">
-          <Zap size={50} className="mb-3 text-warning" />
+          <Zap size={25} className="mb-3 text-warning" />
           <h3>Fast</h3>
           <p>Generate passwords instantly.</p>
         </div>
 
         <div className="col-md-4">
-          <ClipboardCopy size={50} className="mb-3 text-success" />
+          <ClipboardCopy size={25} className="mb-3 text-success" />
           <h3>Easy Copy</h3>
           <p>Copy passwords with one click.</p>
         </div>
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 function PasswordGenerator() {
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+
+    let newPassword = "";
+    const length = 12; // password length
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      newPassword += chars[randomIndex];
+    }
+
+    setPassword(newPassword);
+  };
+
   return (
     <div className="container">
       <div className="card generator-card shadow-lg">
@@ -7,16 +26,21 @@ function PasswordGenerator() {
 
           <input
             className="form-control mb-3"
+            value={password}
             placeholder="Generated Password"
+            readOnly
           />
 
-          <button className="btn btn-primary w-100">
+          <button
+            className="btn btn-primary w-100"
+            onClick={generatePassword}
+          >
             Generate Password
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PasswordGenerator
+export default PasswordGenerator;
